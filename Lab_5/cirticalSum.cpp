1fc5d9a1e8366f7a85cef6d7d68672f6f0603d4a
EtshD1 <mohamed.h.abouelenin@gmail.com>
2024-05-19 13:42:52 +0300

    Better formatting

--- Lab_5/cirticalSum.cpp
+++ Lab_5/cirticalSum.cpp
@@ -19,10 +19,10 @@ void criticalSum()
 	int a1[size] = {0};
 	int a2[size] = {0};
 	int a3[size] = {0};
-    
+
 	int sum = 0;
-    
-    clock_t start_time = clock(); // Start Timer
+
+	clock_t start_time = clock(); // Start Timer
 
 	// Filling a1 and a2 using parallelization
 #pragma omp parallel for
@@ -37,19 +37,16 @@ void criticalSum()
 	for (int i = 0; i < size; i++)
 		a3[i] = a1[i] * a2[i];
 
-	
-    // Summation. Share variables are sum and a3
-#pragma omp parallel for shared(a3, sum)
+#pragma omp parallel for shared(a3, sum) // Summation. Share variables are sum and a3
 	for (int i = 0; i < size; i++)
-#pragma omp critical // Ensure that the following is executed by one thread at a time
+#pragma omp critical  // Ensure that the following is executed by one thread at a time
 		sum += a3[i]; // sum of array 3 element
-    
-    
-    clock_t end_time = clock(); // End Timer
-    
-    double execution_time = static_cast<double>(end_time - start_time) / CLOCKS_PER_SEC; // Time in seconds
-    
-    std::cout << "Sum: " << sum << std::endl;
-    
-    std::cout << "Execution Time: " << execution_time << std::endl;
+
+	clock_t end_time = clock(); // End Timer
+
+	double execution_time = static_cast<double>(end_time - start_time) / CLOCKS_PER_SEC; // Time in seconds
+
+	std::cout << "Sum: " << sum << std::endl;
+
+	std::cout << "Execution Time: " << execution_time << std::endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include "./Lab_1/searchArr.hpp"
 #include "./Lab_2/calcPi.hpp"
 #include "./Lab_3/parallelArraySearch.hpp"
+#include "./Lab_5/cirticalSum.hpp"
 
 int main() {
-    // Lab 1
-    // searchArray();
     // Lab 2
-    calculatePi();
+    // searchArray();
     // Lab 3
+    //calculatePi();
+    // Lab 4
     // parallelArraySearch();
+    // Lab 5
+    criticalSum();
     return 0;
 }
