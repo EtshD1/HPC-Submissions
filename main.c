bbda2c639fe037766e478f83c7e79b6663d8de4a
EtshD1 <mohamed.h.abouelenin@gmail.com>
2024-05-19 20:02:42 +0300

    Lab 7

--- main.c
+++ main.c
@@ -1,78 +1,44 @@
-// OpenMP Labs
-// #include "./Lab_1/searchArr.hpp"
-// #include "./Lab_2/calcPi.hpp"
-// #include "./Lab_3/parallelArraySearch.hpp"
-// #include "./Lab_5/cirticalSum.hpp"
 
 #include <stdio.h>
-
-// MPI
 #include "mpi.h"
-
-// Lab 6
-void nodeCommunication()
-{
-    // Initialize MPI
-    MPI_Init(NULL, NULL);
-
-    int size;
-    MPI_Comm_size(MPI_COMM_WORLD, &size); // Get Size
-
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Get Rank
-
-    // Define variables
-    int data = 2, sender, receiver;
-
-    // Receive from 3, send to 1
-    if (rank == 0)
-    {
-        sender = 3;
-        receiver = 1;
-        data *= 2;
-        MPI_Send(&data, 1, MPI_INT, receiver, 0, MPI_COMM_WORLD);
-        printf("Data = %d\n", data);
-        MPI_Recv(&data, 1, MPI_INT, sender, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-    // Receive from 0, send to 1
-    if (rank == 1)
-    {
-        sender = 0;
-        receiver = 2;
-        MPI_Recv(&data, 1, MPI_INT, sender, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        data *= 2;
-        MPI_Send(&data, 1, MPI_INT, receiver, 0, MPI_COMM_WORLD);
-        printf("Data = %d\n", data);
-    }
-    // Receive from 1, send to 3
-    if (rank == 2)
-    {
-        sender = 1;
-        receiver = 3;
-        MPI_Recv(&data, 1, MPI_INT, sender, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        data *= 2;
-        MPI_Send(&data, 1, MPI_INT, receiver, 0, MPI_COMM_WORLD);
-        printf("Data = %d\n", data);
-    }
-    // Receive from 2, send to 0
-    if (rank == 3)
-    {
-        sender = 2;
-        receiver = 0;
-        MPI_Recv(&data, 1, MPI_INT, sender, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        data *= 2;
-        MPI_Send(&data, 1, MPI_INT, receiver, 0, MPI_COMM_WORLD);
-        printf("Data = %d\n", data);
-    }
-
-    // Finish our MPI work
-    MPI_Finalize();
-}
 
 int main()
 {
-    nodeCommunication();
+    MPI_Init(NULL, NULL);
+    int size, id;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+
+    if (id == 0)
+    {
+        int Data[15] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        MPI_Send(&Data[5], 5, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        MPI_Send(&Data[10], 5, MPI_INT, 2, 0, MPI_COMM_WORLD);
+        int localsum = 0;
+        for (int i = 0; i < 5; i++)
+            localsum += Data[i];
+
+        MPI_Status X_Status;
+        int sum1, sum2;
+        MPI_Recv(&sum1, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &X_Status);
+        MPI_Recv(&sum2, 1, MPI_INT, 2, 0, MPI_COMM_WORLD, &X_Status);
+        printf("Process of ID: %d\n", id);
+        printf("Total sum of array: %d\n\n", localsum + sum1 + sum2);
+    }
+    else if (id <= 2)
+    {
+        int Data[5];
+        MPI_Status X_Status;
+        MPI_Recv(&Data, 5, MPI_INT, 0, 0, MPI_COMM_WORLD, &X_Status);
+        int localsum = 0;
+        for (int i = 0; i < 5; i++)
+            localsum += Data[i];
+
+        MPI_Send(&localsum, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        printf("Process of ID: %d\n", id);
+        printf("Total sum of array: %d\n\n", localsum);
+    }
+
+    MPI_Finalize();
     return 0;
 }
-
-// MPI Labs
